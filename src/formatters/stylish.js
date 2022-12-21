0af9c0ee4e2ba94be865ca0e6e4920fd884cba2c
root <root@LAPTOP-4T1UH53D.localdomain>
2022-12-21 20:37:57 +0300

    commit

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 
-const indent = (depth) => ' '.repeat(depth * 4 - 2);
+const indent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
 
 const stringify = (value, depth = 1) => {
   if (!_.isObject(value)) {
-    return value;
+    return String(value);
   }
   const keys = Object.keys(value);
   const output = keys.map((key) => `${indent(depth + 1)}  ${key}: ${stringify(value[key], depth + 1)}`);
@@ -12,18 +12,18 @@ const stringify = (value, depth = 1) => {
 };
 
 const iter = (tree, depth) => tree.map((node) => {
-  const getValue = (value, sign) => `${indent(depth)}${sign} ${node.key}: ${stringify(value, depth)}\n`;
+  const output = (children) => iter(children, depth + 1);
   switch (node.type) {
     case 'added':
       return `${indent(depth)}${'+'} ${node.key}: ${stringify(node.value, depth)}\n`;
     case 'deleted':
       return `${indent(depth)}${'-'} ${node.key}: ${stringify(node.value, depth)}\n`;
     case 'updated':
-      return `${getValue(node.value1, '-')}${getValue(node.value2, '+')}`;
+      return `${indent(depth)}${'-'} ${node.key}: ${stringify(node.value1, depth)}\n${indent(depth)}${'+'} ${node.key}: ${stringify(node.value2, depth)}\n`;
     case 'complex':
-      return `${indent(depth)}  ${node.key}: {\n${iter(node.children, depth + 1).join('')}${indent(depth)}  }\n`;
+      return `${indent(depth)}  ${node.key}: {\n${output(node.children).join('')}${indent(depth)}  }\n`;
     default:
-      return getValue(node.value, ' ');
+      return `${indent(depth)}${' '} ${node.key}: ${stringify(node.value, depth)}\n`;
   }
 });
 
