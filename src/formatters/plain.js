be97dd5a4c06706901b3d6364200946258873c15
valentina <Anitnelav01@yandex.ru>
2022-12-31 19:46:37 +0300

    commit

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,19 +10,21 @@ const stringify = (value) => {
   return String(value);
 };
 
-const iter = (nodes, parent) => nodes.filter((node) => node.type !== 'unchanged').map((node) => {
-  const property = parent ? `${parent}.${node.key}` : node.key;
-  switch (node.type) {
-    case 'added':
-      return `Property '${property}' was added with value: ${stringify(node.value)}`;
-    case 'deleted':
-      return `Property '${property}' was removed`;
-    case 'updated':
-      return `Property '${property}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
-    default:
-      return `${iter(node.children, property)}`;
-  }
-}).join('\n');
+const iter = (nodes, parent) => nodes
+  .filter((node) => node.type !== 'unchanged')
+  .map((node) => {
+    const property = parent ? `${parent}.${node.key}` : node.key;
+    switch (node.type) {
+      case 'added':
+        return `Property '${property}' was added with value: ${stringify(node.value)}`;
+      case 'deleted':
+        return `Property '${property}' was removed`;
+      case 'updated':
+        return `Property '${property}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
+      default:
+        return `${iter(node.children, property)}`;
+    }
+  }).join('\n');
 
 const formatPlain = (diff) => iter(diff, '');
 
